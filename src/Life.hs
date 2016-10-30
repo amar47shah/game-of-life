923ffd075ea9c737536efc811a38ae0634c539ed
Amar Shah <amar47shah@gmail.com>
2016-10-30 01:57:31 -0400

    Add Gosper glider gun pattern

--- src/Life.hs
+++ src/Life.hs
@@ -1,10 +1,10 @@
 module Life (Life, life, isLive) where
 
 import Life.Life (Life, generation, isLive)
-import Life.Pattern (backrake)
+import Life.Pattern (gosperGliderGun)
 
 life :: [Life]
 life = iterate generation start
 
 start :: Life
-start = backrake
+start = gosperGliderGun
--- src/Life/Pattern.hs
+++ src/Life/Pattern.hs
@@ -3,6 +3,7 @@ module Life.Pattern ( backrake
                     , blinkerShip
                     , canadaGoose
                     , collision
+                    , gosperGliderGun
                     , glider
                     , unknownFormation
                     ) where
@@ -57,6 +58,14 @@ canadaGoose = shift (12,12) $ fromCells
 
 collision :: Life
 collision = together unknownFormation $ shift (2,-2) glider
+
+gosperGliderGun :: Life
+gosperGliderGun = shift (-15, -10) $ fromCells
+  [ (0,4),(0,5),(1,4),(1,5),(10,4),(10,5),(10,6),(11,3),(11,7)
+  , (12,2),(12,8),(13,2),(13,8),(14,5),(15,3),(15,7),(16,4),(16,5),(16,6)
+  , (17,5),(20,2),(20,3),(20,4),(21,2),(21,3),(21,4),(22,1),(22,5)
+  , (24,0),(24,1),(24,5),(24,6),(34,2),(34,3),(35,2),(35,3)
+  ]
 
 glider :: Life
 glider = shift (12,-8) $ fromCells [(-1,-1),(-1,0),(-1,1),(0,1),(1,0)]
